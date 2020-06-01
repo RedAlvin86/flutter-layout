54db2ae1e63937cda81783815c4cebde881aec80
seok86 <redseok@tapick.net>
2020-06-01 21:09:24 +0900

    Test

---
+++ android/app/src/main/kotlin/com/test/flutterdeb/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.test.flutterdeb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+          width: 100,
+          height: 100,
+          padding: const EdgeInsets.all(8.0),
+          margin: const EdgeInsets.all(8.0),
+        ),
+    );
+  }
+}
+
+
